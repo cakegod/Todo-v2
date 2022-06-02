99d3befe94bd49db0db9f83f3a37f2afe056bc7d
Cakegod <cakemancerr@gmail.com>
2022-06-02 20:21:12 +0200

    added task displayer logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './normalize.css';
 import { DOM, menu, modal, taskDisplayer } from './modules/DOM';
 
 import { Project, Task } from './modules/classes';
+import { currentTask } from '../../Todo/src/modules/task';
 
 const projectArray = [];
 const newProject = new Project('home', []);
@@ -16,22 +17,41 @@ projectArray.push(newProject3);
 
 let currentProject = 'home';
 
-let index = projectArray.findIndex((array) => array.title === currentProject);
+let currentProjectIndex = projectArray.findIndex(
+	(array) => array.title === currentProject,
+);
+let currentTaskIndex;
+let currentTaskContainer;
+let currentObject;
 
-// Open modal
-DOM.newTask.addEventListener('click', () => {
+////// MODAL
+
+//// SUBMIT
+// Event
+DOM.newTask.addEventListener('click', addSubmitEvent);
+// Open modal and add an event to the submit to add a new task
+function addSubmitEvent() {
+	modal.form.addEventListener('submit', createTask);
+	openModal();
+}
+
+//// OPEN
+function openModal() {
 	modal.form.classList.add('active');
-});
+}
 
-// Close modal
+//// CLOSE
+// Event to the close button, removes all submit events
 modal.close.addEventListener('click', () => {
-	modal.form.classList.remove('active');
-});
+	modal.form.removeEventListener('submit', createTask);
+	modal.form.removeEventListener('submit', submitEdit);
 
-// Submit modal
-modal.submit.addEventListener('click', (event) => {
-	createTask(event);
+	closeModal();
 });
+// Close modal
+function closeModal() {
+	modal.form.classList.remove('active');
+}
 
 function createTask(event) {
 	event.preventDefault();
@@ -45,7 +65,7 @@ function createTask(event) {
 }
 
 function pushTask(newTask) {
-	projectArray[index].array.push(newTask);
+	projectArray[currentProjectIndex].array.push(newTask);
 	console.log(projectArray);
 	renderTask(newTask);
 }
@@ -63,29 +83,13 @@ function renderTask(newTask) {
 	checkbox.textContent = 'radio_button_unchecked';
 	title.textContent = newTask.title;
 
-	function taskColor() {
-	switch (newTask.priority) {
-		case 'low':
-			task.className = 'task low-priority';
-			break;
-		case 'medium':
-			task.className = 'task medium-priority';
-			break;
-		case 'high':
-			task.className = 'task high-priority';
-			break;
-		default:
-			task.className = 'task low-priority';
-	}
-}
-
 	function checkboxToggle() {
 		if (newTask.checkbox) {
 			checkbox.textContent = 'radio_button_checked';
 			task.className = 'task checked-priority';
 			newTask.checkbox = false;
 		} else {
-			taskColor();
+			taskColor(newTask, task);
 			checkbox.textContent = 'radio_button_unchecked';
 			newTask.checkbox = true;
 		}
@@ -98,9 +102,33 @@ function renderTask(newTask) {
 	});
 
 	task.addEventListener('click', () => {
-		displayTaskDetails(newTask)
-	})
+		displayTaskDetails(newTask);
+		currentTaskIndex = projectArray[currentProjectIndex].array.indexOf(newTask);
+		currentTaskContainer = { task, checkbox, title };
+		currentObject = projectArray[currentProjectIndex].array[currentTaskIndex];
+		console.log(
+			currentTaskIndex,
+			currentProjectIndex,
+			projectArray[currentProjectIndex].array[currentTaskIndex],
+			currentTaskContainer,
+		);
+	});
+}
 
+function taskColor(object, container) {
+	switch (object.priority) {
+		case 'low':
+			container.className = 'task low-priority';
+			break;
+		case 'medium':
+			container.className = 'task medium-priority';
+			break;
+		case 'high':
+			container.className = 'task high-priority';
+			break;
+		default:
+			container.className = 'task low-priority';
+	}
 }
 
 function displayTaskDetails(newTask) {
@@ -108,28 +136,46 @@ function displayTaskDetails(newTask) {
 	taskDisplayer.description.textContent = newTask.description;
 	taskDisplayer.date.textContent = newTask.date;
 
-	if (newTask.priority === "high") {
-		taskDisplayer.priority.style.color = "var(--red-high)";
-	} else if (newTask.priority === "medium") {
-		taskDisplayer.priority.style.color = "var(--yellow-med)";
+	if (newTask.priority === 'high') {
+		taskDisplayer.priority.style.color = 'var(--red-high)';
+	} else if (newTask.priority === 'medium') {
+		taskDisplayer.priority.style.color = 'var(--yellow-med)';
 	} else {
-		taskDisplayer.priority.style.color = "var(--green-low)";
-	}
-function deleteTaskButton () {
-	if (currentTask) {
-		projectArray[currentProjectN].array.splice(
-			projectArray[currentProjectN].array.indexOf(currentTask),
-			1
-		);
-		console.log(projectArray);
-		currentTaskContainer.remove();
-		taskDisplayer.title.textContent = "";
-		taskDisplayer.description.textContent = "";
-		taskDisplayer.date.textContent = "";
+		taskDisplayer.priority.style.color = 'var(--green-low)';
 	}
 }
 
+taskDisplayer.delete.addEventListener('click', () => {
+	deleteTask();
+});
+
+function deleteTask() {
+	projectArray[currentProjectIndex].array.splice(currentTaskIndex, 1);
+	currentTaskContainer.task.remove();
+	console.log(projectArray);
 }
 
+taskDisplayer.edit.addEventListener('click', () => {
+	editTask();
+});
 
+function editTask() {
+	openModal();
+	modal.form.addEventListener('submit', submitEdit);
+	modal.title.value = currentObject.title;
+	modal.description.value = currentObject.description;
+	modal.date.value = currentObject.date;
+	modal.priority.value = currentObject.priority;
+}
 
+function submitEdit(event) {
+	event.preventDefault();
+	currentObject.title = modal.title.value;
+	currentObject.description = modal.description.value;
+	currentObject.date = modal.date.value;
+	currentObject.priority = modal.priority.value;
+	currentTaskContainer.title.textContent = modal.title.value;
+	taskColor(currentObject, currentTaskContainer.task);
+
+	console.log(currentObject, projectArray);
+}
